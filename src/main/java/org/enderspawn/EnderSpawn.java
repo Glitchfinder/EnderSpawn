2ea00309aa2f156b1d953dc6ce911f35574f67b8
U-YUE\Sean <glitchkey@gmail.com>
2013-02-06 10:31:20 -0800

    Updated copyright dates.

--- src/main/java/org/enderspawn/EnderSpawn.java
+++ src/main/java/org/enderspawn/EnderSpawn.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Sean Porter <glitchkey@gmail.com>
+ * Copyright (c) 2012-2013 Sean Porter <glitchkey@gmail.com>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
